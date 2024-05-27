b6a2ec9d6f6f017a6a8471edf7e1db2a679e77ed
itsik-bamani <112100680+itsik-ba@users.noreply.github.com>
2024-05-27 11:45:35 +0300

    builder

--- client/src/components/CreateTeam.tsx
+++ client/src/components/CreateTeam.tsx
@@ -1,12 +1,48 @@
-
+import "../index.css"
 
 
 const CreateTeam = () => {
   return (
     <>
     <main>
-     <header>
+    <div className="text-center py-2">
+  <h1>create your team</h1>
+   </div> 
+
+     <header className="py-6" >
+
+
+
+<div className="flex justify-center gap-2">
+       <div>
+        <input 
+        type="text"
+        placeholder="member name"
+        className="bg-dragMe w-44 text-textColor outline-0 font-semibold text-lg text-center placeholder-custom " />
+      </div>
       
+      <div>
+        <input 
+        type="text" 
+        placeholder="role" 
+        className="bg-dragMe w-44 text-textColor font-semibold text-lg outline-0 text-center placeholder-custom " />
+      </div>
+ </div>
+
+      <div className="w-screen flex justify-center py-4">
+        <input 
+        type="text"
+        placeholder="mission"
+        className="bg-dragMe w-44 text-textColor outline-0 
+        text-center font-semibold text-lg placeholder-custom"/>
+        
+      </div>
+
+      <div className="text-center py-2">
+      <button className="bg-mainPink py-1 px-2">add</button>
+      </div>
+    
+
      </header>
     </main>
     </>
