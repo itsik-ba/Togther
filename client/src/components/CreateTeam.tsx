aba3994600e36c6d7a306aebf6c102b56d8e3594
itsik-bamani <112100680+itsik-ba@users.noreply.github.com>
2024-06-03 16:16:51 +0300

    I

--- client/src/components/CreateTeam.tsx
+++ client/src/components/CreateTeam.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "../index.css"
+
 
 interface CreateTeamProps {
   memberInfo: {
@@ -14,22 +16,36 @@ interface CreateTeamProps {
 }
 
 
-const CreateTeam: React.FC<CreateTeamProps> = ({ memberInfo, setMemberInfo }) => {
-
-
+const CreateTeam: React.FC<CreateTeamProps> = ({ setMemberInfo }) => {
+    
+  const [localInfo, setLocalInfo] = useState({ member: '', role: '', mission: '' });
+  const [errors, setErrors] = useState('');
 
  const handleSubmitMember = () => {
-  console.log(memberInfo)
+
+  try {
+  if(!localInfo.role || !localInfo.member || !localInfo.mission){
+    throw new Error("Please fill out all fields");
+  } else{
+    setMemberInfo(localInfo);
+    setLocalInfo({ member: '', role: '', mission: '' }); 
+    setErrors(""); 
+  } 
+  } catch (error:any) {
+    console.error(error.message);
+    setErrors(error.message);
+  }
+  
  }
 
 
-
+console.log(errors)
 
   return (
     <>
     <main>
     <div className="text-center py-2">
-  <h1>create your team</h1>
+      <h1>create your team</h1> 
    </div> 
 
      <header className="py-6" >
@@ -39,10 +55,11 @@ const CreateTeam: React.FC<CreateTeamProps> = ({ memberInfo, setMemberInfo }) =>
 <div className="flex justify-center gap-2">
        <div>
         <input 
+        
         type="text"
         name="member"
-        value={memberInfo.member}
-        onChange={(e) => setMemberInfo({ ...memberInfo, member: e.target.value })}
+        value={localInfo.member}
+        onChange={(e) => setLocalInfo({ ...localInfo, member: e.target.value })}
         placeholder="member name"
         className="bg-dragMe w-44 text-mainPink outline-0 font-semibold text-lg text-center p-1 placeholder-custom" />
       </div>
@@ -51,8 +68,8 @@ const CreateTeam: React.FC<CreateTeamProps> = ({ memberInfo, setMemberInfo }) =>
         <input 
         type="text" 
         name="role"
-        value={memberInfo.role}
-        onChange={(e) => setMemberInfo({ ...memberInfo, role: e.target.value })}
+        value={localInfo.role}
+        onChange={(e) => setLocalInfo({ ...localInfo, role: e.target.value })}
         placeholder="role" 
         className="bg-dragMe w-44 text-textColor font-semibold text-lg outline-0 text-center placeholder-custom p-1" />
       </div>
@@ -62,8 +79,8 @@ const CreateTeam: React.FC<CreateTeamProps> = ({ memberInfo, setMemberInfo }) =>
         <input 
         type="text"
         name="mission"
-        value={memberInfo.mission}
-        onChange={(e) => setMemberInfo({ ...memberInfo, mission: e.target.value })}
+        value={localInfo.mission}
+        onChange={(e) => setLocalInfo({ ...localInfo, mission: e.target.value })}
         placeholder="mission"
         className="bg-dragMe w-44 text-textColor outline-0 
         text-center font-semibold text-lg placeholder-custom p-1"/>
@@ -76,9 +93,13 @@ const CreateTeam: React.FC<CreateTeamProps> = ({ memberInfo, setMemberInfo }) =>
       onClick={handleSubmitMember}
       className="bg-mainPink py-1 px-2 text-textColor">add</button>
       </div>
-    
+
+    <div className="text-center py-3">
+      {errors ? <h1 className="text-erorrs">{errors}</h1> : null}      
+    </div>
 
      </header>
+   
     </main>
     </>
    
